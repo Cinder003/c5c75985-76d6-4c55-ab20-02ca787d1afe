f95d983ae7c206089934e16211050cdc903abf7d
Abraham James <abrahamjames2808003@gmail.com>
2025-11-14 11:25:54 +0530

    Add Front-end/tailwind.config.js

---
+++ Front-end/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+      animation: {
+        'gradient-bg': 'gradient-bg 15s ease infinite',
+      },
+      keyframes: {
+        'gradient-bg': {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center',
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center',
+          },
+        },
+      },
+    },
+  },
+  plugins: [],
+}
